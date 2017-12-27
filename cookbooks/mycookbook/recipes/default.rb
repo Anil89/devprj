c2870cad04b040ec8a205e808c35056fc86d9145
root <root@ip-172-31-15-182.ap-south-1.compute.internal>
2017-12-27 04:20:13 +0000

    updated mycoookbook

--- cookbooks/mycookbook/recipes/default.rb
+++ cookbooks/mycookbook/recipes/default.rb
@@ -6,3 +6,20 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+directory '/tmp/dir1' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
+
+file '/tmp/dir1/index.php' do
+  content '<html>This is a placeholder for the home page.</html>'
+  mode '0755'
+end
+
+package 'httpd' 
+
+service 'httpd' do 
+   action :start
+end
